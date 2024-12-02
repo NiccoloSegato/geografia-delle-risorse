46cb67756a9b85ad9a63c35829e52408f1375b9e
niccolosegato <segato117@gmail.com>
2024-12-02 23:40:57 +0100

    First UI

--- sketch.js
+++ sketch.js
@@ -6,6 +6,12 @@ let expensesLength;
 let regions = ["Tutte le regioni"];
 let categories = [];
 
+/**
+ * Colors
+ */
+let backgroundColor = "#252129";
+let textColor = "#ffffff";
+
 function preload() {
   data = loadTable('assets/dataset/uscite.csv', 'ssv', 'header');
 }
@@ -31,33 +37,43 @@ function setup() {
       categories.push(category);
     }
   }
-
-  console.log(categories);
 }
 
 function draw() {
-  background(220);
+  background(backgroundColor);
 
-  text(regions.length, 100, 50);
+  fill(textColor);
+  textSize(50);
+  textFont('Georgia');
+  textStyle(BOLD);
+  text("La geografia delle risorse", 50, 80);
+  textSize(20);
+  textFont('forma-djr-text');
+  textStyle(NORMAL);
+  text("Differenze regionali nelle spese pubbliche", 50, 120);
+  textFont('Ortica');
+  textSize(18);
+  text("Testo descrittivo con delle informazioni sul progetto\nDa modificare e definire", 50, 160);
+  textFont('forma-djr-text');
 
-  let x = 100;
-  let y = 100;
+  let y = 230;
 
-  fill("black");
-  textSize(windowWidth/45);
+  let labelDistance = (windowHeight - y - 50)/20;
   
+  // Stampo "Tutte le regioni"
+  let allRegionsLabel = createP(regions[0]);
+  allRegionsLabel.id('selected-label');
+  allRegionsLabel.addClass('region-label');
+  allRegionsLabel.position(50, y);
+
+  y += labelDistance;
+
   // Stampo i nomi delle regioni
-  for (let i = 0; i < regions.length; i++) {
-    text(regions[i], x, y);
-    y += windowWidth/40;
-  }
+  for (let i = 1; i < regions.length; i++) {
+    let regionLabel = createP(regions[i]);
+    regionLabel.addClass('region-label');
+    regionLabel.position(50, y);
 
-  // Stampo le categorie di spesa
-  x = 300;
-  y = 100;
-
-  for (let i = 0; i < categories.length; i++) {
-    text(categories[i], x, y);
-    y += windowWidth/40;
+    y += labelDistance
   }
 }
