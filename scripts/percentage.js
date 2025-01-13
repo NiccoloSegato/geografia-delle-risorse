0bf6e16f6254091aed0e63b91676cdfba224de35
niccolosegato <segato117@gmail.com>
2025-01-13 10:43:01 +0100

    New percentage script in index

--- scripts/percentage.js
+++ scripts/percentage.js
@@ -5,5 +5,53 @@
  * 
  * Authors: Bano, Chinni, Lages, Magalhaes, Menoni, Piganzoli, Segato
  * 
- * Questo script carica la rappresentazione grafica usando il framework p5.js
+ * Questo script calcola i valori da rappresentare per la visualizzazione in percentuale
+ * 
+ * FUNZIONAMENTO DEL CALCOLO
+ * Ogni regione ha un numero di pallini che rappresenta il 100% delle spese.
+ * Viene quindi calcolata la percentuale per ogni categoria per ogni regione e il numero di pallini da rappresentare.
  */
+
+// In questo array verranno messi, in ordine di categoria, i valori percentuali
+let percentageData = [];
+
+/**
+ * Funzione per calcolare i valori percentuali
+ */
+function pLoadPercentages() {
+    // Iterazione per ogni regione
+    for (let i = 0; i < regions.length; i++) {
+        let region = regions[i];
+        let regionData = [];
+        let regionTotal = regionDataLastYear[regions.indexOf(selectedRegion) - 1].data[i].amount
+
+        // Iterazione per ogni categoria
+        for (let j = 0; j < categories.length; j++) {
+            let category = categories[j];
+            let categoryTotal = 0;
+
+            // Iterazione per ogni spesa
+            for (let k = 0; k < expensesLength; k++) {
+                let expense = expenses[k];
+                if (expense['Regione per Dettaglio'] === region && expense['Categoria'] === category) {
+                    categoryTotal += parseInt(expense['Importo']);
+                }
+            }
+
+            regionData.push(categoryTotal);
+            regionTotal += categoryTotal;
+        }
+
+        // Calcolo dei valori percentuali
+        let regionPercentage = [];
+        for (let j = 0; j < regionData.length; j++) {
+            regionPercentage.push(regionData[j] / regionTotal * 100);
+        }
+
+        percentageData.push(regionPercentage);
+    }
+
+    console.log(percentageData);
+}
+
+
--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,3 +1,14 @@
+/** 
+ * GEOGRAFIA DELLE REGIONI
+ * Politecnico di Milano - AA 2024/25
+ * Design della Comunicazione - Laboratorio di Computer Grafica per l'Information Design
+ * 
+ * Authors: Bano, Chinni, Lages, Magalhaes, Menoni, Piganzoli, Segato
+ * 
+ * Questo script si occupa di gestire il funzionamento del toggle per la visualizzazione in percentuale
+ */
+
+
 const descriptions = [
     "100.000.000€",
     "0,0087%"
@@ -12,4 +23,8 @@ function tToggleView() {
     activeOption = 1 - activeOption; // Alterna tra 0 e 1
     toggleButton.classList.toggle("active", activeOption === 1);
     descriptionText.textContent = descriptions[activeOption];
+
+    if(activeOption === 1 && percentageData.length === 0) {
+        pLoadPercentages();
+    }
 }
