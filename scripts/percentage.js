41773b60881bc53485e7f6ccd1ae8e67e9e5e7b0
niccolosegato <segato117@gmail.com>
2025-01-13 10:21:47 +0100

    New percentage script and Bano surname typo

---
+++ scripts/percentage.js
@@ -0,0 +1,9 @@
+/** 
+ * GEOGRAFIA DELLE REGIONI
+ * Politecnico di Milano - AA 2024/25
+ * Design della Comunicazione - Laboratorio di Computer Grafica per l'Information Design
+ * 
+ * Authors: Bano, Chinni, Lages, Magalhaes, Menoni, Piganzoli, Segato
+ * 
+ * Questo script carica la rappresentazione grafica usando il framework p5.js
+ */
